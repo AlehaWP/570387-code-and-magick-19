eba68b4432e0c4e771a0dcb5ab6f56b4b7e20ed6
Нерченко Алексей <a.nerchenko@ctm.ru>
2020-01-24 17:45:52 +0300

    Исправлен стиль кода

--- js/stat.js
+++ js/stat.js
@@ -12,25 +12,25 @@ var figureMargin = 50;
 
 var renderCloud = function (ctx, x, y, color, width, height) {
   ctx.fillStyle = color;
-  ctx.fillRect (x, y, width, height);
-}
+  ctx.fillRect(x, y, width, height);
+};
 
 window.renderStatistics = function (ctx, names, times) {
   var paddingLeft = CLOUD_X + 2 * GAP;
 
-  renderCloud (ctx, CLOUD_X + GAP, CLOUD_Y  + GAP, 'rgba(0, 0, 0, 0.7)', CLOUD_WIDTH, CLOUD_HEIGHT);
-  renderCloud (ctx, CLOUD_X, CLOUD_Y, '#fff', CLOUD_WIDTH, CLOUD_HEIGHT);
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)', CLOUD_WIDTH, CLOUD_HEIGHT);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff', CLOUD_WIDTH, CLOUD_HEIGHT);
 
-  ctx.font = 'none '+textHeight+'px PT Mono';
-  ctx.fillStyle = "#000";
-  ctx.textBaseline = "top";
+  ctx.font = 'none ' + textHeight + 'px PT Mono';
+  ctx.fillStyle = '#000';
+  ctx.textBaseline = 'top';
 
-  ctx.fillText("Ура вы победили!", paddingLeft, CLOUD_Y + 2*GAP);
-  ctx.fillText("Список результатов:", paddingLeft, CLOUD_Y + 3*GAP + textHeight);
+  ctx.fillText('Ура вы победили!', paddingLeft, CLOUD_Y + 2 * GAP);
+  ctx.fillText('Список результатов:', paddingLeft, CLOUD_Y + 3 * GAP + textHeight);
   paddingLeft = CLOUD_X + 4 * GAP;
   for (var i = 0; i < names.length; i++) {
-    ctx.fillText(names[i], paddingLeft, bottomY - 3*GAP);
+    ctx.fillText(names[i], paddingLeft, bottomY - 3 * GAP);
     paddingLeft += (figureWidth + figureMargin);
   }
 
-}
+};
