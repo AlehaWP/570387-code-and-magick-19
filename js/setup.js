dfdbe85674027c45595daeec28b32ace8e5d023a
Нерченко Алексей <a.nerchenko@ctm.ru>
2020-01-28 14:19:07 +0300

    Убрано значение по умолчанию

--- js/setup.js
+++ js/setup.js
@@ -7,24 +7,24 @@ var showElement = function (selector) {
   return element;
 };
 
-var returnRandomIndex = function (arrLength = 10) {
-  return Math.floor(Math.random() * arrLength)
-}
+var returnRandomIndex = function (arrLength) {
+  return Math.floor(Math.random() * arrLength);
+};
 
 var createWizard = function () {
   var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф',
-                    'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+    'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
   var surNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц',
-                  'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+    'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)',
-                    'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+    'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
   return {
     name: firstNames[returnRandomIndex(firstNames.length)] + ' ' + surNames[returnRandomIndex(surNames.length)],
     coatColor: coatColors[returnRandomIndex(coatColors.length)],
     eyesColor: eyesColors[returnRandomIndex(eyesColors.length)]
-  }
-}
+  };
+};
 
 var createWizardsArr = function (quantity) {
   var resultArr = [];
@@ -32,14 +32,14 @@ var createWizardsArr = function (quantity) {
     resultArr[i] = createWizard();
   }
   return resultArr;
-}
+};
 
 var createWizardElement = function (elementTemplate, objWizard) {
   elementTemplate.querySelector('.setup-similar-label').textContent = objWizard.name;
   elementTemplate.querySelector('.wizard-coat').style.fill = objWizard.coatColor;
   elementTemplate.querySelector('.wizard-eyes').style.fill = objWizard.eyesColor;
   return elementTemplate;
-}
+};
 
 var wizarsdsQuantity = 4;
 var setupWindow = showElement('.setup');
@@ -50,7 +50,7 @@ var wizardTemplate = document.querySelector('#similar-wizard-template')
 
 var wizards = createWizardsArr(wizarsdsQuantity);
 
-var fragment = document.createDocumentFragment()
+var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizards.length; i++) {
   fragment.appendChild(createWizardElement(wizardTemplate.cloneNode(true), wizards[i]));
 }
