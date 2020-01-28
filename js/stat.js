d1da64c8166c4e9d2e8af909e7e6d70b0e12a923
Нерченко Алексей <a.nerchenko@ctm.ru>
2020-01-28 14:03:01 +0300

    Приведено к соответствию с критерием Б8

--- js/stat.js
+++ js/stat.js
@@ -2,16 +2,29 @@
 
 var GAP = 10;
 var TEXT_HEIGHT = 16;
+var Player = {
+  NAME: 'Вы',
+  COLOR: 'rgba(255, 0, 0, 1)'
+};
 
-var CLOUD = {
-  width: 420,
-  height: 270,
-  x: 100,
-  y: 10,
-  contentX: 120,
-  contentY: 30,
-  contentRightX: 500,
-  contentBottomY: 240
+var Cloud = {
+  WIDTH: 420,
+  HEIGHT: 270,
+  X: 100,
+  Y: 10,
+  CONTENT_X: 120,
+  CONTENT_Y: 30,
+  CONTENT_RIGHT_X: 500,
+  CONTENT_BOTTOM_Y: 240,
+  COLOR: '#fff',
+  SHADOW_COLOR: 'rgba(0, 0, 0, 0.7)'
+};
+
+var Column = {
+  WIDTH: 40,
+  MAX_HEIGHT: 150 - GAP - TEXT_HEIGHT,
+  MARGIN_RIGHT: 50,
+  BASE_COLOR: '240'
 };
 
 var writeInCloud = function (ctx, x, y, text) {
@@ -23,10 +36,10 @@ var writeInCloud = function (ctx, x, y, text) {
 
 // Функция рисования  облака
 var renderCloud = function (ctx) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(CLOUD.x + GAP, CLOUD.y + GAP, CLOUD.width, CLOUD.height);
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(CLOUD.x, CLOUD.y, CLOUD.width, CLOUD.height);
+  ctx.fillStyle = Cloud.SHADOW_COLOR;
+  ctx.fillRect(Cloud.X + GAP, Cloud.Y + GAP, Cloud.WIDTH, Cloud.HEIGHT);
+  ctx.fillStyle = Cloud.COLOR;
+  ctx.fillRect(Cloud.X, Cloud.Y, Cloud.WIDTH, Cloud.HEIGHT);
 };
 
 var getMaxValue = function (arr) {
@@ -35,44 +48,39 @@ var getMaxValue = function (arr) {
 
 var getRandomColor = function () {
   var saturationValue = Math.round(Math.random() * 100);
-  return 'hsl(240,' + saturationValue + '%,50%)';
+  return 'hsl(' + Column.BASE_COLOR + ',' + saturationValue + '%,50%)';
 };
 
 
 var getColumnColorByName = function (name) {
-  if (name.toLowerCase() === 'вы') {
-    return 'rgba(255, 0, 0, 1)';
+  if (name.toLowerCase() === Player.NAME.toLowerCase()) {
+    return Player.COLOR;
   }
   return getRandomColor();
 };
 
 var paintHistogram = function (ctx, names, times) {
-  var columnWidth = 40;
-  var columnMargin = 50;
-  var histogramHeight = 150;
-  var placeForColumnsHeight = histogramHeight - GAP - TEXT_HEIGHT;
   var maxTime = getMaxValue(times);
-  var oneSecInPixel = placeForColumnsHeight / maxTime;
-  var cursorX = CLOUD.contentX + 2 * GAP;
-  var cursorY = CLOUD.contentBottomY;
+  var oneSecInPixel = Column.MAX_HEIGHT / maxTime;
+  var cursorX = Cloud.CONTENT_X + 2 * GAP;
   // Все рисуется от низа отсюда - в перемещении курсора по Y
   for (var i = 0; i < names.length; i++) {
     var columnHeight = oneSecInPixel * times[i];
+    var cursorY = Cloud.CONTENT_BOTTOM_Y;
     writeInCloud(ctx, cursorX, cursorY, names[i]);
     ctx.fillStyle = getColumnColorByName(names[i]);
     cursorY -= GAP;
-    ctx.fillRect(cursorX, cursorY, columnWidth, -columnHeight);
+    ctx.fillRect(cursorX, cursorY, Column.WIDTH, -columnHeight);
     cursorY -= (2 * GAP + columnHeight);
     writeInCloud(ctx, cursorX, cursorY, Math.round(times[i]));
     // Передвигаем курсор на новую точку для рисования следующей колонки
-    cursorX += columnWidth + columnMargin;
-    cursorY = CLOUD.contentBottomY;
+    cursorX += Column.WIDTH + Column.MARGIN_RIGHT;
   }
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  var cursorX = CLOUD.contentX;
-  var cursorY = CLOUD.contentY;
+  var cursorX = Cloud.CONTENT_X;
+  var cursorY = Cloud.CONTENT_Y;
   renderCloud(ctx);
   writeInCloud(ctx, cursorX, cursorY, 'Ура вы победили!');
   cursorY = cursorY + TEXT_HEIGHT + GAP;
